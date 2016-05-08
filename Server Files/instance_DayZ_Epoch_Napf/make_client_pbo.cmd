662694feeafb945a5fc363c81ed05faee384dcfb
Bruce <bruce@playz.rocks>
2016-05-08 17:00:51 +0200

    Updated client build script to include mod.cpp and anzio

--- Server Files/instance_DayZ_Epoch_Napf/make_client_pbo.cmd
+++ Server Files/instance_DayZ_Epoch_Napf/make_client_pbo.cmd
@@ -17,22 +17,28 @@ Set ClientCode=%home%\SQF
 
 
 net use \\TESTSERVER\dayz_mod /user:administrator Ad.1234
-copy "%Deploy%\dayz_anim.pbo" \\TESTSERVER\dayz_mod\@DayZ_Epoch106\Addons\
-copy "%Deploy%\dayz_code.pbo" \\TESTSERVER\dayz_mod\@DayZ_Epoch106\Addons\
-copy "%Deploy%\dayz_epoch_b.pbo" \\TESTSERVER\dayz_mod\@DayZ_Epoch106\Addons\
-copy "%Deploy%\dayz_sfx.pbo" \\TESTSERVER\dayz_mod\@DayZ_Epoch106\Addons\
-::copy "%Deploy%\redryder.pbo" \\TESTSERVER\dayz_mod\@DayZ_Epoch106\Addons\
+copy "%ClientCode%\mod.cpp" "\\TESTSERVER\dayz_mod\@DayZ_Epoch106\"
+copy "%Deploy%\dayz_anim.pbo" "\\TESTSERVER\dayz_mod\@DayZ_Epoch106\Addons\"
+copy "%Deploy%\dayz_code.pbo" "\\TESTSERVER\dayz_mod\@DayZ_Epoch106\Addons\"
+copy "%Deploy%\dayz_epoch_b.pbo" "\\TESTSERVER\dayz_mod\@DayZ_Epoch106\Addons\"
+copy "%Deploy%\dayz_sfx.pbo" "\\TESTSERVER\dayz_mod\@DayZ_Epoch106\Addons\"
+::copy "%Deploy%\redryder.pbo" "\\TESTSERVER\dayz_mod\@DayZ_Epoch106\Addons\"
+::copy "%Deploy%\anzio.pbo" "\\TESTSERVER\dayz_mod\@DayZ_Epoch106\Addons\"
 
+copy "%ClientCode%\mod.cpp" "C:\Program Files (x86)\Steam\steamapps\common\ARMA 2 Operation Arrowhead\@DayZ_Epoch106\"
 copy "%Deploy%\dayz_anim.pbo" "C:\Program Files (x86)\Steam\steamapps\common\ARMA 2 Operation Arrowhead\@DayZ_Epoch106\addons\"
 copy "%Deploy%\dayz_code.pbo" "C:\Program Files (x86)\Steam\steamapps\common\ARMA 2 Operation Arrowhead\@DayZ_Epoch106\addons\"
 copy "%Deploy%\dayz_epoch_b.pbo" "C:\Program Files (x86)\Steam\steamapps\common\ARMA 2 Operation Arrowhead\@DayZ_Epoch106\addons\"
 copy "%Deploy%\dayz_sfx.pbo" "C:\Program Files (x86)\Steam\steamapps\common\ARMA 2 Operation Arrowhead\@DayZ_Epoch106\addons\"
 ::copy "%Deploy%\redryder.pbo" "C:\Program Files (x86)\Steam\steamapps\common\ARMA 2 Operation Arrowhead\@DayZ_Epoch106\addons\"
+::copy "%Deploy%\anzio.pbo" "C:\Program Files (x86)\Steam\steamapps\common\ARMA 2 Operation Arrowhead\@DayZ_Epoch106\addons\"
 
+copy "%ClientCode%\mod.cpp" "\\r2\c$\Program Files (x86)\Steam\steamapps\common\ARMA 2 Operation Arrowhead\@DayZ_Epoch106\"
 copy "%Deploy%\dayz_anim.pbo" "\\r2\c$\Program Files (x86)\Steam\steamapps\common\ARMA 2 Operation Arrowhead\@DayZ_Epoch106\addons\"
 copy "%Deploy%\dayz_code.pbo" "\\r2\c$\Program Files (x86)\Steam\steamapps\common\ARMA 2 Operation Arrowhead\@DayZ_Epoch106\addons\"
 copy "%Deploy%\dayz_epoch_b.pbo" "\\r2\c$\Program Files (x86)\Steam\steamapps\common\ARMA 2 Operation Arrowhead\@DayZ_Epoch106\addons\"
 copy "%Deploy%\dayz_sfx.pbo" "\\r2\c$\Program Files (x86)\Steam\steamapps\common\ARMA 2 Operation Arrowhead\@DayZ_Epoch106\addons\"
 ::copy "%Deploy%\redryder.pbo" "\\r2\c$\Program Files (x86)\Steam\steamapps\common\ARMA 2 Operation Arrowhead\@DayZ_Epoch106\addons\"
+::copy "%Deploy%\anzio.pbo" "\\r2\c$\Program Files (x86)\Steam\steamapps\common\ARMA 2 Operation Arrowhead\@DayZ_Epoch106\addons\"
 
 
